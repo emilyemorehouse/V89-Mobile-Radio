02041f7137dd90398dc713b63e7c0721f3687249
Emily Morehouse <emilyemorehouse@gmail.com>
2013-01-22 22:36:20 -0500

    some layout added

--- gen/edu/wvfs/fsu/v89mobileradio/R.java
+++ gen/edu/wvfs/fsu/v89mobileradio/R.java
@@ -12,16 +12,24 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
-        public static final int ic_tab_radio_active=0x7f020001;
-        public static final int ic_tab_radio_default=0x7f020002;
-        public static final int tab_icon_radio=0x7f020003;
+        public static final int ic_tab_list_active=0x7f020001;
+        public static final int ic_tab_list_default=0x7f020002;
+        public static final int ic_tab_radio_active=0x7f020003;
+        public static final int ic_tab_radio_default=0x7f020004;
+        public static final int ic_tab_v89=0x7f020005;
+        public static final int tab_icon_list=0x7f020006;
+        public static final int tab_icon_radio=0x7f020007;
     }
     public static final class id {
+        public static final int ImageButton01=0x7f070007;
+        public static final int ImageButton02=0x7f070006;
         public static final int RelativeLayout1=0x7f070000;
-        public static final int menu_settings=0x7f070004;
-        public static final int tab1=0x7f070001;
-        public static final int tab2=0x7f070002;
-        public static final int tab3=0x7f070003;
+        public static final int button_calendar=0x7f070005;
+        public static final int button_radio=0x7f070004;
+        public static final int content=0x7f070002;
+        public static final int handle=0x7f070003;
+        public static final int mainlayout=0x7f070001;
+        public static final int menu_settings=0x7f070008;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -31,6 +39,7 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int button_description=0x7f040003;
         public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
     }
--- src/edu/wvfs/fsu/v89mobileradio/MainActivity.java
+++ src/edu/wvfs/fsu/v89mobileradio/MainActivity.java
@@ -2,16 +2,17 @@ package edu.wvfs.fsu.v89mobileradio;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.graphics.Color;
 import android.view.Menu;
-
+import android.view.View;
 public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		
+		View mlayout = findViewById(R.id.mainlayout);
+		mlayout.setBackgroundColor(Color.DKGRAY);
 	}
 
 	@Override
