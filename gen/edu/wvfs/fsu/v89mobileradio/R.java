35beb8cde63e48b1558c6607781469b30f71a85f
Emily Morehouse <emilyemorehouse@gmail.com>
2013-03-20 10:44:16 -0400

    fixed static string for calendar to res string (can be set to call from db)

--- gen/edu/wvfs/fsu/v89mobileradio/R.java
+++ gen/edu/wvfs/fsu/v89mobileradio/R.java
@@ -48,6 +48,7 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f040000;
         public static final int button_description=0x7f040003;
+        public static final int calendar_text=0x7f040004;
         public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
     }
