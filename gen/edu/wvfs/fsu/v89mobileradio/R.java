ab157bb999769b3320e674b4f4c00c076f68525a
Emily Morehouse <emilyemorehouse@gmail.com>
2013-03-26 11:58:22 -0400

    fixed contact fragment

--- gen/edu/wvfs/fsu/v89mobileradio/R.java
+++ gen/edu/wvfs/fsu/v89mobileradio/R.java
@@ -11,30 +11,32 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int ic_tab_list_active=0x7f020001;
-        public static final int ic_tab_list_default=0x7f020002;
-        public static final int ic_tab_radio_active=0x7f020003;
-        public static final int ic_tab_radio_default=0x7f020004;
-        public static final int ic_tab_v89=0x7f020005;
-        public static final int tab_icon_list=0x7f020006;
-        public static final int tab_icon_radio=0x7f020007;
+        public static final int ic_contact_background=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int ic_tab_list_active=0x7f020002;
+        public static final int ic_tab_list_default=0x7f020003;
+        public static final int ic_tab_radio_active=0x7f020004;
+        public static final int ic_tab_radio_default=0x7f020005;
+        public static final int ic_tab_v89=0x7f020006;
+        public static final int tab_icon_list=0x7f020007;
+        public static final int tab_icon_radio=0x7f020008;
     }
     public static final class id {
-        public static final int RadioLayout=0x7f070006;
+        public static final int RadioLayout=0x7f070007;
         public static final int RelativeLayout1=0x7f070000;
-        public static final int button_browse=0x7f07000c;
-        public static final int button_calendar=0x7f07000b;
-        public static final int button_contact=0x7f07000d;
-        public static final int button_radio=0x7f07000a;
+        public static final int button_browse=0x7f07000d;
+        public static final int button_calendar=0x7f07000c;
+        public static final int button_contact=0x7f07000e;
+        public static final int button_radio=0x7f07000b;
         public static final int calendarpanel=0x7f070003;
         public static final int content_fragment=0x7f070002;
-        public static final int contentpanel=0x7f070004;
-        public static final int menu_settings=0x7f07000e;
-        public static final int notification_content=0x7f070005;
-        public static final int playPause=0x7f070007;
-        public static final int preparing_bar=0x7f070008;
-        public static final int tabpanel=0x7f070009;
+        public static final int contentpanel=0x7f070005;
+        public static final int imageView1=0x7f070004;
+        public static final int menu_settings=0x7f07000f;
+        public static final int notification_content=0x7f070006;
+        public static final int playPause=0x7f070008;
+        public static final int preparing_bar=0x7f070009;
+        public static final int tabpanel=0x7f07000a;
         public static final int tabs_fragment=0x7f070001;
     }
     public static final class layout {
